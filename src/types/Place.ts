d061be06c9bdcd978c83990d3b88ceb0cd369f53
hwank1 <kijh4383@gmail.com>
2026-01-17 16:23:36 +0900

    feat : 맛집 , 명소 mockData 처리 후 타입 지정

---
+++ src/types/Place.ts
@@ -0,0 +1,11 @@
+type PlaceType = "restaurant" | "attraction";
+
+export type Place = {
+  id: number;
+  name: string;
+  rating: number;
+  address: string;
+  image: string;
+  category: string;
+  type: PlaceType;
+};
